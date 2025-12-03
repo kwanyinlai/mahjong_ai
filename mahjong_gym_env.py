d105c109123c4d24d89d9b3976f725fc54bb9206
kwanyinlai <laikwanyin06@gmail.com>
2025-12-03 02:34:02 -0500

    complete the adapter interface

--- mahjong_gym_env.py
+++ mahjong_gym_env.py
@@ -18,11 +18,11 @@ class MahjongEnviornmentAdapter(gym.Env):
         self.controlling_player_id = controlling_player_id
 
         self.action_space = spaces.Discrete(18)
-        # 0 - 13: discard tiles 1 to 14
+        # 0 - 13: discard tiles 0 to 13
         # 14 claim win
-        # 15 claim pong
-        # 16 claim sheung
-        # 17 claim kong
+        # 15 claim kong
+        # 16 claim pong
+        # 17 claim sheung
 
         self.observation_space = spaces.Box(
             low=0.0,
@@ -32,24 +32,70 @@ class MahjongEnviornmentAdapter(gym.Env):
         )
 
     def get_observation(self):
+        """
+        Return the state of the game
+        :return:
+        """
         state = self.game.get_state()
         return np.array(state, dtype=np.float32)
 
     def reset(self):
+        """
+        Reset the game state
+        :return:
+        """
         self.game.setup_game()
         return self.get_observation()
 
     def step(self, action: int):
         """
-        Execute a single step
-        :return:
+        A single step
         """
-        player = self.game.players[self.controlling_player_id]
-        assert player is RLAgent
+        rl_player = self.game.players[self.controlling_player_id]
+        assert isinstance(rl_player, RLAgent)
 
-        if self.controlling_player_id is not self.game.current_player_no:
-            self.game.play_turn()
+        reward = 0.0
+        info = {}
 
+        if self.game.current_player == rl_player:
+            if action < len(rl_player.hidden_hand):
+                tile_to_discard = rl_player.hidden_hand[action]
+                self.game.discard_tile(rl_player, self.game.get_state(), tile=tile_to_discard)
+            else:
+                action_str = self._map_int_to_action(action)
+                self.game.execute_interrupt(self.controlling_player_id, action_str)
+                if action_str == "win":
+                    reward = 1.0
+                    self.game.game_over = True
+                elif action_str in ["pong", "kong"]:
+                    reward = 0.1
+                elif action_str == "sheung":
+                    reward = -0.1
+
+            if not self.game.game_over:
+                self.game.next_turn()
+                self.game.draw_tile(self.game.current_player)
+
+            obs = self.get_observation()
+            return obs, reward, self.game.game_over, info
+
+        else:
+            actioning_player_id, suggested_action = self.game.play_turn()
+            if actioning_player_id is not None and actioning_player_id != self.controlling_player_id:
+                self.game.execute_interrupt(actioning_player_id, suggested_action)
+
+            if self.game.current_player == rl_player or self.game.game_over:
+                obs = self.get_observation()
+                done = self.game.game_over
+                return obs, reward, done, info
+
+            if actioning_player_id is None:
+                self.game.next_turn()
+                self.game.draw_tile(self.game.current_player)
+
+        obs = self.get_observation()
+        done = self.game.game_over
+        return obs, reward, done, info
 
 
     def render(self, mode='human'):
@@ -59,5 +105,14 @@ class MahjongEnviornmentAdapter(gym.Env):
     def close(self):
         pass
 
-    def get_valid_actions(self):
-        return self.game.get_valid_actions()
+    def _map_int_to_action(self, action) -> str:
+        match action:
+            case 14:
+                return "win"
+            case 15:
+                return "kong"
+            case 16:
+                return "pong"
+            case 17:
+                return "sheung"
+        raise ValueError("Invalid action type")
