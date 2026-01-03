3bda353013d3832d47df4f402078ec6744b4884c
kwanyinlai <laikwanyin06@gmail.com>
2026-01-03 21:45:18 +0800

    add episode_memory as opposed to updating during training

--- reinforcement_learning/training.py
+++ reinforcement_learning/training.py
@@ -1,13 +1,8 @@
-import numpy as np
-import torch
-
-from mahjong_environment.ai_bot import YesBot
-from mahjong_gym_env import MahjongEnvironmentAdapter
-
 from mahjong_environment.mahjong_game import MahjongGame
+from mahjong_environment.mahjong_game_adapter import MahjongEnvironmentAdapter
 from reinforcement_learning.neural_network import PolicyValueNetwork
 from reinforcement_learning.rl_bot import RLAgent
-from reinforcement_learning.self_play_rl_agent import MahjongModel
+from reinforcement_learning.decision_model import MahjongModel
 
 
 class Training:
@@ -21,7 +16,9 @@ class Training:
         Run a training loop of num episodes
         :return:
         """
-        network = PolicyValueNetwork(MahjongGame.state_size, hidden_layer_size=20)
+        network = PolicyValueNetwork(state_size=MahjongGame.state_size,
+                                     action_space=21,  # TODO: rdefine in constant
+                                     hidden_layer_size=20)
         decision_model = MahjongModel(network=network)
 
         ai0 = RLAgent(player_id=0, player_order=0, mahjong_model=decision_model)
@@ -41,51 +38,71 @@ class Training:
         )
         total_rewards = [0, 0, 0, 0]
 
-        for episode in range(self.num_episodes):
+        for _ in range(self.num_episodes):
+            episode_memory = [[], [], [], []]
             state = env.reset()  # Reset the environment at the start of each episode
             done = False
             prev_scores = [player.score for player in players]
 
             while not done and len(env.game.tiles) > 0:
-                # ====================== NON-DISCARD PHASE =======================
-                legal_actions = [[], [], [], []]
-                for i in range(len(players)):
-                    player = players[i]
-                    our_turn = env.game.current_player == player
-                    legal_actions[i] = env.game.get_legal_actions(discard_turn=False, our_turn=our_turn, player=player)
-
-                selected_actions = [player.select_actions(legal_actions, state) for player in players]
-
-                next_state, rewards, done = env.step(selected_actions)
-
-                for i in range(4):
-                    player = players[i]
-                    player.push_experience((state, selected_actions[i], rewards[i], next_state, done))
-                    player.update_model()
-                state = next_state
-                total_rewards = [total_rewards[i] + rewards[i] for i in range(4)]
-
-                # ====================== DISCARD PHASE =======================
+                # ====================== ====================== ======================
+                # ====================== DISCARD PHASE ===========================
+                # ====================== ====================== ======================
                 legal_actions = [[], [], [], []]
                 for i in range(len(players)):
                     player = players[i]
                     our_turn = env.game.current_player == player
                     legal_actions[i] = env.game.get_legal_actions(discard_turn=True, our_turn=our_turn, player=player)
 
-                selected_actions = [player.select_actions(legal_actions, state) for player in players]
+                selected_actions = [player.select_actions(legal_actions[i], state) for i, player in enumerate(players)]
+                # TODO: Mask public state before passing to decision model
 
-                next_state, rewards, done = env.step(selected_actions)
+                next_state, done = env.step_with_all_actions(selected_actions)
 
-                for i in range(4):
-                    player = players[i]
-                    player.push_experience((state, selected_actions[i], rewards[i], next_state, done))
-                    player.update_model()
+                if env.game.last_acting_player is not None:
+                    actioning_player_id = env.game.last_acting_player.player_id
+                    episode_memory[actioning_player_id].append(
+                        (state, selected_actions[actioning_player_id])
+                    )
+
                 state = next_state
-                total_rewards = [total_rewards[i] + rewards[i] for i in range(4)]
+
+                # ====================== ====================== ======================
+                # ====================== NON-DISCARD PHASE ===========================
+                # ====================== ====================== ======================
+                legal_actions = [[], [], [], []]
+                for i in range(len(players)):
+                    player = players[i]
+                    our_turn = env.game.current_player == player
+                    legal_actions[i] = env.game.get_legal_actions(discard_turn=False, our_turn=our_turn, player=player)
+
+                selected_actions = [player.select_actions(legal_actions[i], state) for player in players]
+
+                next_state, done = env.step_with_all_actions(selected_actions)
+
+                if env.game.last_acting_player is not None:
+                    actioning_player_id = env.game.last_acting_player.player_id
+                    episode_memory[actioning_player_id].append(
+                        (state, selected_actions[actioning_player_id][1])
+                    )
 
                 if env.game.game_over:  # If the game is over, break early
                     continue
 
                 done = env.game.game_over
+            print("WIN? " + str(env.game.game_over))
+            #  TODO: check if our game is terminating properly. we aren't getting any messages
+            #  TODO: about a game draw even though the game terminates this way
+            final_rewards = [
+                players[i].score - prev_scores[i]
+                for i in range(4)
+            ]
 
-            total_rewards += [players[i].score - prev_scores[i] for i in range(4)]
+            for i in range(4):
+                for (s, a) in episode_memory[i]:
+                    decision_model.push_experience((s, a, final_rewards[i]))
+
+            decision_model.update_model()
+
+            for i in range(4):
+                total_rewards[i] += final_rewards[i]
